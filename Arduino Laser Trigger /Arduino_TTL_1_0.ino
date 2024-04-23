098daab088e45b6c4afac97cc28348b38cb76443
donglabimaging <50585879+donglabimaging@users.noreply.github.com>
2024-04-23 16:33:47 -0400

    Arduino

---
+++ Arduino Laser Trigger /Arduino_TTL_1_0.ino
@@ -0,0 +1,28 @@
+// activates or assigns TTL to pin 8 
+const int TTL4=8;
+int value=0;  
+
+void setup() 
+   { 
+      // Initializes serial communication at a baud rate of 9600.
+      Serial.begin(9600); 
+      pinMode(TTL4, OUTPUT);
+      digitalWrite (TTL4, LOW);
+      Serial.println("Connection established...");
+   }
+ 
+void loop() 
+   {
+     while (Serial.available())
+        {
+           value = Serial.read();
+        }
+     
+     // If the received value is 'O', it sets pin 8 (TTL4) to HIGH.
+     if (value == 'O')
+        digitalWrite (TTL4, HIGH);
+     
+     // If the received value is 'X', it sets pin 8 (TTL4) to LOW.
+     else if (value == 'X')
+        digitalWrite (TTL4, LOW);
+   } 
